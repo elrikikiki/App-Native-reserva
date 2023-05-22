06f3835178716b9b9f21d0af51d2f7e5c67d6ad2
elrikikiki <santizanini1@gmail.com>
2023-05-22 14:19:17 -0300

    petition agregada

--- src/components/FormularioReserva/FormReserv.jsx
+++ src/components/FormularioReserva/FormReserv.jsx
@@ -5,24 +5,37 @@ import AntDesign from "react-native-vector-icons/AntDesign";
 import Axios from "../../api/Axios";
 import moment from 'moment';
 
-    /* const NEWRESERVATION_URL = "http://localhost:3500/api/reservation/newReservation"; */
-    const NEWRESERVATION_URL = "api/restaurant/prueba";
     const FormReserv = ({item}) => {
+    const NEWRESERVATION_URL = `api/reservation/newReservation/${item._id}`; 
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
     const [numOfPeople, setNumOfPeople] = useState('');
+    const [specificPetition, setSpecificPetition] = useState('');
     const [selectedDate, setSelectedDate] = useState('');
     const [selectedTime, setSelectedTime] = useState('');
-    
+    const [reservStatus, setReservStatus] = useState('')
+      
     const handleSubmit = async (e) => {
     e.preventDefault()
-    
       try {
-        await Axios.post(NEWRESERVATION_URL, 
-          JSON.stringify ({
-            "prueba": "345"
-            })
-            );
+        await Axios.post(
+          NEWRESERVATION_URL,
+          JSON.stringify({
+            "userId": "2132157441",
+            "date": date,
+            "user": "Santi Zanini",
+            "time": time,
+            "peopleQty": numOfPeople,
+            "promotionCode": "no",
+            /* "state": "active" */
+          }),
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+         .then(function (response) {
+          setReservStatus(response.data);
+        }) 
       } catch (error) {
         console.log(error);
       }
@@ -34,10 +47,10 @@ import moment from 'moment';
     "user": "Angel Di María",
     "time": time,
     "peopleQty": numOfPeople,
-    "promotion": "no"
+    "promotionCode": "no",
+    "petition": specificPetition //AVISARLE AL FAFA
     });
     };
-  /* `Id: ${item.restarurantId}, Restaurant: ${item.name}, Date: ${date}, Time: ${time}, Number of People: ${peopleQty}` */
   const handleDayPress = (day) => {
   const selectedDateObj = moment(day.timestamp).utc().startOf('day'); //tener en cuenta esto cuando subamos al servidor
   setSelectedDate(day.dateString);
@@ -54,29 +67,41 @@ import moment from 'moment';
     return (
       <View>
         <Calendar
+        style={{marginTop:30}}
           onDayPress={handleDayPress}
           markedDates={{ [selectedDate]: { selected: true } }}
         />
-        <View>
-          <Text>Time</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <Button title="10:00 AM" onPress={() => handleTimePress('10:00 AM')} />
-            <Button title="12:00 PM" onPress={() => handleTimePress('12:00 PM')} />
-            <Button title="2:00 PM" onPress={() => handleTimePress('2:00 PM')} />
-            <Button title="4:00 PM" onPress={() => handleTimePress('4:00 PM')} />
+        <View style={{marginTop:10, alignItems:'center'}}>
+          <Text>Horarios Disponibles</Text>
+          <View style={{ flexDirection: 'row', marginTop:10 }}>
+            <Button color={'#8b0000'} title="10:00 AM" onPress={() => handleTimePress('10')} />
+            <Button color={'#8b0000'} title="12:00 PM" onPress={() => handleTimePress('12')} />
+            <Button color={'#8b0000'} title="2:00 PM" onPress={() => handleTimePress('2')} />
+            <Button color={'#8b0000'} title="4:00 PM" onPress={() => handleTimePress('4')} />
           </View>
         </View>
-        <View>
+        <View style={{marginTop:10}}>
           <TextInput
-            placeholder="Number of People"
+            placeholder="Cantidad de Personas"
             keyboardType="numeric"
             onChangeText={(value) => setNumOfPeople(value)}
             value={numOfPeople}
           />
         </View>
-        <View>
-          <Button title="Enviar" onPress={handleSubmit} />
+        <View style={{marginTop:10}}>
+          <TextInput
+            placeholder="Dejar alguna petición en particular..."
+            keyboardType="text"
+            onChangeText={(value) => setSpecificPetition(value)}
+            value={specificPetition}
+          />
         </View>
+        <View style={{ padding:40}}>
+          <Button color={'#8b0000'} title="Enviar" onPress={handleSubmit} />
+          <Alert></Alert>
+          <Text>{reservStatus}</Text>
+        </View>
+
       </View>
     );
   };
